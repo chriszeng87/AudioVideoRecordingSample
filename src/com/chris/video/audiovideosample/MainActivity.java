57f45027fc4ba883e4031db7003550fc6b63753f
zengxuhong <zengxuhong@kingsoft.com>
2016-01-08 11:26:26 +0800

    make preview full screen

--- src/com/chris/video/audiovideosample/MainActivity.java
+++ src/com/chris/video/audiovideosample/MainActivity.java
@@ -26,6 +26,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.serenegiant.mediaaudiotest.R;
 
@@ -34,7 +36,11 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
 		setContentView(R.layout.activity_main);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
